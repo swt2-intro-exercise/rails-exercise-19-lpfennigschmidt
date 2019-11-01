81ebedd281d53e416e98c32a90dcccc0209e117d
Lara Pfennigschmidt <lara.pfennigschmidt@student.hpi.de>
2019-11-01 18:28:22 +0100

    can now create new authors via form #3

---
+++ spec/features/author/author_spec.rb
@@ -0,0 +1,14 @@
+
+describe "Create new Author", type: :model do 
+	it "should create a new author with given name and homepage" do
+		first_name = 'Alan'
+		last_name = 'Turing'
+		homepage = 'http://wikipedia.org/Alan_Turing'
+
+		author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+		expect(author.first_name).to eq(first_name)
+		expect(author.last_name).to eq(last_name)
+		expect(author.homepage).to eq(homepage)
+
+	end
+end
