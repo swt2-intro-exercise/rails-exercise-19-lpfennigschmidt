731fdf151fbbda630a8d113be6c124d1dfbd4f55
Lara Pfennigschmidt <lara.pfennigschmidt@student.hpi.de>
2019-11-02 12:26:30 +0100

    shows error messages for invalid data in form for #6

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,7 @@
 class Author < ApplicationRecord
-	validates :first_name, presence: true,
-                    length: { minimum: 1 }
-    validates :last_name, presence: true,
-                    length: { minimum: 1 }
-    validates :homepage, presence: true,
-                    length: { minimum: 1 }
+	validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :homepage, presence: true
 
 	def name
 		first_name + " " + last_name
