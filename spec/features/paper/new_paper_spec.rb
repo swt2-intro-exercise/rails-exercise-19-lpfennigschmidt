df2953b0002a48675965b84bd4ff219bbd71d0a8
Lara Pfennigschmidt <lara.pfennigschmidt@student.hpi.de>
2019-11-06 11:04:14 +0100

    added more validation for papers for #11

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -15,9 +15,34 @@ describe "New paper page", type: :feature do
 
   it "should error when entering no title" do
   	visit new_paper_path
-  	fill_in('paper[venue]', with: 'HPI')
+  	fill_in 'paper[venue]', with: 'HPI'
   	fill_in 'paper[year]', with: '1998'
   	click_button('Create Paper')
   	expect(page).to have_text('error')
+  end
+
+  it "should error when entering no venue" do
+    visit new_paper_path
+    fill_in 'paper[title]', with: 'Title'
+    fill_in 'paper[year]', with: '1998'
+    click_button('Create Paper')
+    expect(page).to have_text('error')
+  end
+
+  it "should error when entering no year" do
+    visit new_paper_path
+    fill_in 'paper[title]', with: 'Title'
+    fill_in 'paper[venue]', with: 'HPI'
+    click_button('Create Paper')
+    expect(page).to have_text('error')
+  end
+
+  it "should error when entering no number as year" do
+    visit new_paper_path
+    fill_in 'paper[title]', with: 'Title'
+    fill_in 'paper[venue]', with: 'HPI'
+    fill_in 'paper[year]', with: 'eins'
+    click_button('Create Paper')
+    expect(page).to have_text('error')
   end
 end
